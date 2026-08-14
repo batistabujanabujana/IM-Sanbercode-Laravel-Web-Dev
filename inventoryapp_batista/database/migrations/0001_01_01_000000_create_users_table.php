59a17f1aa4836c6be0bb1d63f46a428e1b9f7ac1
batistabujanabujana <bujanabatista@gmail.com>
2026-08-14 07:24:47 +0700

    Tugas 14 - Migration Database Selesai

--- inventoryapp_batista/database/migrations/0001_01_01_000000_create_users_table.php
+++ inventoryapp_batista/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 255);
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 255);
+            
+            // Tambahkan kolom 'role' sesuai ERD tugas
+            $table->enum('role', ['staff', 'admin'])->default('staff');
+            
             $table->rememberToken();
             $table->timestamps();
         });
